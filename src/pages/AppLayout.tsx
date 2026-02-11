67031c4e43a4b16492ae7d4aa473b6a711778008
numaan <numaankazi145@gmail.com>
2026-02-11 12:59:45 +0530

    to be tested

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -19,17 +19,6 @@ export function AppLayout() {
     <div className="flex min-h-screen">
       {token && (
         <SideMenu sidemenuIcon1="Overview.png" sidemenuName1="Overview" />
-        // <SideMenu
-        //   sidemenuIcon1="sideIcon1.png"
-        //   sidemenuIcon2="sideIcon2.png"
-        //   sidemenuIcon3="sideIcon3.png"
-        //   sidemenuIcon4="sideIcon4.png"
-        //   // ------Names---------
-        //   sidemenuName1="Dashboard"
-        //   sidemenuName2="Participant"
-        //   sidemenuName3="Competency"
-        //   sidemenuName4="Leadership Level"
-        // />
       )}
 
       <main className="w-full bg-[#8E9FC11F]">
